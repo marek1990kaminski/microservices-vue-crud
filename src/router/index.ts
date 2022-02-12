bdc6d1ffbb6132ab19313966c05bc10263cc33d7
jon <marek1990kaminski@gmail.com>
2022-02-12 17:07:14 +0100

    some routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Admin from '../pages/admin/Admin.vue';
+import Products from '../pages/admin/Products.vue';
+import Main from '../pages/Main.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
+    component: Main,
+  },
+  {
+    path: '/admin',
     component: Admin,
+    children: [
+      {
+        path: 'products',
+        component: Products,
+      },
+    ],
   },
 
 ];
